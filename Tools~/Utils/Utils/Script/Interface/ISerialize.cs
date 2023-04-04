316b2882c19439639763032b580bb25c03366f53
sam <1398581458@qq.com>
2023-04-04 16:54:54 +0800

    更新编辑器二进制数据文件

---
+++ Tools~/Utils/Utils/Script/Interface/ISerialize.cs
@@ -0,0 +1,23 @@
+﻿namespace AIO
+{
+    /// <summary>
+    /// 序列化 反序列化
+    /// </summary>
+    public interface ISerialize
+    {
+        /// <summary>
+        /// 数据
+        /// </summary>
+        byte[] Data { get; }
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        void Serialize();
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        void Deserialize();
+    }
+}
