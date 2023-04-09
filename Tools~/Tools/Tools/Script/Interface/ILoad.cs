a4bb5992d72432469b71bf5172e9005867bee90c
XiNanSky <1398581458@qq.com>
2023-04-09 22:15:20 +0800

    添加Update

---
+++ Tools~/Tools/Tools/Script/Interface/ILoad.cs
@@ -0,0 +1,13 @@
+namespace AIO
+{
+    /// <summary>
+    /// 加载文件等等
+    /// </summary>
+    public interface ILoad
+    {
+        /// <summary>
+        /// 加载
+        /// </summary>
+        void Load();
+    }
+}
