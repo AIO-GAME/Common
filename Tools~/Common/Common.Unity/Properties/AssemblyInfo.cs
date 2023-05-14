44d2804fd6a0bb1de5ad90cfca34f24c722bddc6
XiNanSky <1398581458@qq.com>
2023-05-14 14:54:47 +0800

    修改DLL工程结构

---
+++ Tools~/Common/Common.Unity/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("AIO.Common.Unity")]
+[assembly: AssemblyProduct("AIO.Common.Unity")]
+[assembly: AssemblyDefaultAlias("AIO.Common.Unity")]
+[assembly: AssemblyDescription("AIO Unity Common")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("dc29565f-e2e3-42af-b245-e38eb60914e3")]
+
+[assembly: AssemblyVersion(AssemblyInfo.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfo.FileVersion)]
+[assembly: AssemblyCompany(AssemblyInfo.Company)]
+[assembly: AssemblyCopyright(AssemblyInfo.Copyright)]
+[assembly: AssemblyCulture(AssemblyInfo.Culture)]
+
+[assembly: InternalsVisibleTo("AIO.Common.Unity.Editor")]
---
+++ Tools~/Test/Test.NUnit/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace ALL.Test
+{
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+          
+        }
+    }
+}
