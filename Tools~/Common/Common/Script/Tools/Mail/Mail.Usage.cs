5a85d9b0fba96da6573bd87d9e4a43c50e3502fd
sam <1398581458@qq.com>
2023-05-19 17:44:42 +0800

    更新飘字

---
+++ Tools~/Common/Common/Script/Tools/Mail/Mail.Usage.cs
@@ -0,0 +1,56 @@
+﻿/*|==========|*|
+|*|Author:   |*| -> SAM
+|*|Date:     |*| -> 2023-05-19
+|*|==========|*/
+
+namespace AIO
+{
+    // /// <summary>
+    // /// 邮件系统
+    // /// </summary>
+    // public class MailSystem
+    // {
+    //     /// <summary>
+    //     /// 客户端 一对多发送
+    //     /// </summary>
+    //     public MailMultipleHandle TestMail;
+    //
+    //     /// <summary>
+    //     /// 客户端 链接 服务器
+    //     /// </summary>
+    //     public MailSingleHandle ClientToServers;
+    //
+    //     /// <summary>
+    //     /// 邮件系统
+    //     /// </summary>
+    //     public MailSystem()
+    //     {
+    //         ClientToServers = new MailSingleHandle(new MailSingleInfo
+    //             {
+    //                 Form = "XiNanskyhailun@foxmail.com", //客户端地址
+    //                 FormDisplayName = "夕楠发送器", //可以写入用户昵称
+    //                 FormPort = 25, //端口
+    //                 To = "1398581458@qq.com", //服务器地址
+    //                 Passwrod = "dnonitcgelzmebei", //凭证
+    //                 ClientHost = "smtp.qq.com", //Host地址
+    //             }
+    //             , "问题反馈");
+    //         ClientToServers.Send("hello world 5465465465465465465465", () => { System.Console.WriteLine("发送完成"); });
+    //
+    //         TestMail = new MailMultipleHandle(new MailMultipInfo
+    //         {
+    //             Form = "XiNanskyhailun@foxmail.com", //客户端地址
+    //             FormDisplayName = "测试器", //可以写入用户昵称
+    //             FormPort = 25, //端口
+    //             Passwrod = "dnonitcgelzmebei", //凭证
+    //             ClientHost = "smtp.qq.com", //Host地址
+    //         }, "第二轮测试");
+    //
+    //         TestMail.AddRecipients("1398581458@qq.com", "1143955202@qq.com", "lijie1433223@foxmail.com");
+    //         TestMail.Send(@"自动邮件发送系统测试数据!!!!!!!!
+    //          -1398581458@qq.com
+    //          -XiNan",
+    //             () => { System.Console.WriteLine("发送完成"); });
+    //     }
+    // }
+}
---
+++ Tools~/Common/Common/Script/Tools/Mail/MailSingleHandle.cs
@@ -0,0 +1,77 @@
+﻿/*|============================================|*|
+|*|Author:        |*|XiNan                     |*|
+|*|Date:          |*|2020-05-10                |*|
+|*|E-Mail:        |*|1398581458@qq.com         |*|
+|*|=============================================*/
+
+
+namespace AIO
+{
+    using System;
+    using System.Net;
+    using System.Net.Mail;
+    using System.Text;
+
+
+    /// <summary>
+    /// 单对单 直接发送
+    /// </summary>
+    public class MailSingleHandle : MailHandle
+    {
+        /// <summary>
+        /// 收件人
+        /// </summary>
+        public MailAddress Recipients { get; private set; } = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Info"></param>
+        /// <param name="subject"></param>
+        public MailSingleHandle(MailSingleInfo Info, string subject = "")
+        {
+            Addresser = new MailAddress(Info.Form, Info.FormDisplayName, Info.Encoding);
+
+            SmtpClient = new SmtpClient();
+            SmtpClient.Host = Info.ClientHost; //地址
+            SmtpClient.EnableSsl = Info.ClientEnableSsl; //是否启用SSL
+            SmtpClient.Timeout = Info.ClientTimeout; //超时
+            SmtpClient.DeliveryMethod = SmtpDeliveryMethod.Network; //连接方式
+            SmtpClient.UseDefaultCredentials = false; //默认凭证
+            SmtpClient.Credentials = new NetworkCredential(Addresser.Address, Info.Passwrod); //创建网络凭证
+
+            Recipients = new MailAddress(Info.To, Info.ToDisplayName, Info.Encoding);
+
+            MailMessage = new MailMessage();
+            MailMessage.From = Addresser; //发件人
+            MailMessage.To.Add(Recipients); //收件人
+            MailMessage.IsBodyHtml = true; //是否支持内容为HTML
+            MailMessage.Priority = MailPriority.High; //优先级
+
+            Subject = subject;
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="Body">内容主体</param>
+        /// <param name="CallBack">回调</param>
+        public void Send(string Body, Action CallBack = null)
+        {
+            Send(Subject, Body, CallBack);
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="Label">邮件标题</param>
+        /// <param name="Body">内容主体</param>
+        /// <param name="CallBack">回调</param>
+        public void Send(string Label, string Body, Action CallBack = null)
+        {
+            MailMessage.Subject = Label; //标题
+            MailMessage.Body = Body; //内容
+            SendMail(CallBack);
+        }
+    }
+}
