cb414ee1c26cc58bcab1d8427c57f148012d8f04
sam <1398581458@qq.com>
2023-06-01 12:11:49 +0800

    update webgl jslib

---
+++ Editor/Menu/EditorMenu.cs
@@ -0,0 +1,14 @@
+﻿/*|==========|*|
+|*|Author:   |*| -> SAM
+|*|Date:     |*| -> 2023-05-31
+|*|==========|*/
+
+namespace AIO.Package.Editor
+{
+    /// <summary>
+    /// 编辑器 菜单
+    /// </summary>
+    public static partial class EditorMenu
+    {
+    }
+}
