a31d34dc2b09b1c247a2c17c6f5da0569e82be70
sam <1398581458@qq.com>
2023-06-17 12:10:27 +0800

    新增SRDebuger

---
+++ Dependencies/StompyRobot/SRDebugger/Scripts/UI/Other/SetLayerFromSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SRDebugger.UI.Other
+{
+    using SRF;
+
+    public class SetLayerFromSettings : SRMonoBehaviour
+    {
+        private void Start()
+        {
+            gameObject.SetLayerRecursive(Settings.Instance.DebugLayer);
+        }
+
+    }
+}
