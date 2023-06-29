bfceccde236ee40ca1fa6eefcbf58f59d7cb291d
sam <1398581458@qq.com>
2023-06-29 12:01:47 +0800

    chiyu sam 适配2018.4.4f1

--- Packages~/QHierarchy/Editor/Scripts/pcomponent/QStaticComponent.cs
+++ Packages~/QHierarchy/Editor/Scripts/pcomponent/QStaticComponent.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
 using qtools.qhierarchy.pcomponent.pbase;
 using qtools.qhierarchy.pdata;
-using qtools.qhierarchy.phierarchy;
-using qtools.qhierarchy.phelper;
+using UnityEditor;
+using UnityEngine;
 
 namespace qtools.qhierarchy.pcomponent
 {
-    public class QStaticComponent: QBaseComponent
+    public class QStaticComponent : QBaseComponent
     {
         // PRIVATE
         private Color activeColor;
@@ -25,10 +21,10 @@ namespace qtools.qhierarchy.pcomponent
             rect.width = 11;
             rect.height = 10;
 
-            QSettings.getInstance().addEventListener(QSetting.StaticShow                , settingsChanged);
-            QSettings.getInstance().addEventListener(QSetting.StaticShowDuringPlayMode  , settingsChanged);
-            QSettings.getInstance().addEventListener(QSetting.AdditionalActiveColor     , settingsChanged);
-            QSettings.getInstance().addEventListener(QSetting.AdditionalInactiveColor   , settingsChanged);
+            QSettings.getInstance().addEventListener(QSetting.StaticShow, settingsChanged);
+            QSettings.getInstance().addEventListener(QSetting.StaticShowDuringPlayMode, settingsChanged);
+            QSettings.getInstance().addEventListener(QSetting.AdditionalActiveColor, settingsChanged);
+            QSettings.getInstance().addEventListener(QSetting.AdditionalInactiveColor, settingsChanged);
 
             settingsChanged();
         }
@@ -36,10 +32,10 @@ namespace qtools.qhierarchy.pcomponent
         // PRIVATE
         private void settingsChanged()
         {
-            enabled                     = QSettings.getInstance().get<bool>(QSetting.StaticShow);
+            enabled = QSettings.getInstance().get<bool>(QSetting.StaticShow);
             showComponentDuringPlayMode = QSettings.getInstance().get<bool>(QSetting.StaticShowDuringPlayMode);
-            activeColor                 = QSettings.getInstance().getColor(QSetting.AdditionalActiveColor);
-            inactiveColor               = QSettings.getInstance().getColor(QSetting.AdditionalInactiveColor);
+            activeColor = QSettings.getInstance().getColor(QSetting.AdditionalActiveColor);
+            inactiveColor = QSettings.getInstance().getColor(QSetting.AdditionalInactiveColor);
         }
 
         // DRAW
@@ -67,17 +63,20 @@ namespace qtools.qhierarchy.pcomponent
                 staticButtonColors = new Color32[11 * 10];
             }
 
-            #if UNITY_4_6 || UNITY_4_7
+#if UNITY_4_6 || UNITY_4_7
             drawQuad(39, 5, 4, ((staticFlags & StaticEditorFlags.LightmapStatic       ) > 0));
             drawQuad(33, 5, 4, ((staticFlags & StaticEditorFlags.BatchingStatic       ) > 0));
-            #else   
+#else
+
+#if UNITY_2019_1_OR_NEWER
             drawQuad(37, 3, 4, ((staticFlags & StaticEditorFlags.ContributeGI       ) > 0));
-            drawQuad(33, 3, 4, ((staticFlags & StaticEditorFlags.BatchingStatic       ) > 0));
+#endif
+            drawQuad(33, 3, 4, ((staticFlags & StaticEditorFlags.BatchingStatic) > 0));
             drawQuad(41, 3, 4, ((staticFlags & StaticEditorFlags.ReflectionProbeStatic) > 0));
-            #endif
-            drawQuad( 0, 5, 2, ((staticFlags & StaticEditorFlags.OccludeeStatic       ) > 0));
-            drawQuad( 6, 5, 2, ((staticFlags & StaticEditorFlags.OccluderStatic       ) > 0));
-            drawQuad(88, 5, 2, ((staticFlags & StaticEditorFlags.NavigationStatic     ) > 0));
+#endif
+            drawQuad(0, 5, 2, ((staticFlags & StaticEditorFlags.OccludeeStatic) > 0));
+            drawQuad(6, 5, 2, ((staticFlags & StaticEditorFlags.OccluderStatic) > 0));
+            drawQuad(88, 5, 2, ((staticFlags & StaticEditorFlags.NavigationStatic) > 0));
             drawQuad(94, 5, 2, ((staticFlags & StaticEditorFlags.OffMeshLinkGeneration) > 0));
 
             staticButton.SetPixels32(staticButtonColors);
@@ -91,22 +90,26 @@ namespace qtools.qhierarchy.pcomponent
             {
                 currentEvent.Use();
 
-                int intStaticFlags = (int)staticFlags;
+                var intStaticFlags = (int)staticFlags;
                 gameObjects = Selection.Contains(gameObject) ? Selection.gameObjects : new GameObject[] { gameObject };
 
-                GenericMenu menu = new GenericMenu();
-                menu.AddItem(new GUIContent("Nothing"                   ), intStaticFlags == 0, staticChangeHandler, 0);
-                menu.AddItem(new GUIContent("Everything"                ), intStaticFlags == -1, staticChangeHandler, -1);
-                menu.AddItem(new GUIContent("Lightmap Static"           ), (intStaticFlags & (int)StaticEditorFlags.ContributeGI) > 0, staticChangeHandler, (int)StaticEditorFlags.ContributeGI);
-                menu.AddItem(new GUIContent("Occluder Static"           ), (intStaticFlags & (int)StaticEditorFlags.OccluderStatic) > 0, staticChangeHandler, (int)StaticEditorFlags.OccluderStatic);
-                menu.AddItem(new GUIContent("Batching Static"           ), (intStaticFlags & (int)StaticEditorFlags.BatchingStatic) > 0, staticChangeHandler, (int)StaticEditorFlags.BatchingStatic);
-                menu.AddItem(new GUIContent("Navigation Static"         ), (intStaticFlags & (int)StaticEditorFlags.NavigationStatic) > 0, staticChangeHandler, (int)StaticEditorFlags.NavigationStatic);
-                menu.AddItem(new GUIContent("Occludee Static"           ), (intStaticFlags & (int)StaticEditorFlags.OccludeeStatic) > 0, staticChangeHandler, (int)StaticEditorFlags.OccludeeStatic);
-                menu.AddItem(new GUIContent("Off Mesh Link Generation"  ), (intStaticFlags & (int)StaticEditorFlags.OffMeshLinkGeneration) > 0, staticChangeHandler, (int)StaticEditorFlags.OffMeshLinkGeneration);
-                #if UNITY_4_6 || UNITY_4_7
-                #else
-                menu.AddItem(new GUIContent("Reflection Probe Static"   ), (intStaticFlags & (int)StaticEditorFlags.ReflectionProbeStatic) > 0, staticChangeHandler, (int)StaticEditorFlags.ReflectionProbeStatic);
-                #endif
+                var menu = new GenericMenu();
+                menu.AddItem(new GUIContent("Nothing"), intStaticFlags == 0, staticChangeHandler, 0);
+                menu.AddItem(new GUIContent("Everything"), intStaticFlags == -1, staticChangeHandler, -1);
+#if UNITY_2019_1_OR_NEWER
+                menu.AddItem(new GUIContent("Lightmap Static"), (intStaticFlags & (int)StaticEditorFlags.ContributeGI) > 0, staticChangeHandler, (int)StaticEditorFlags.ContributeGI);
+#endif
+                menu.AddItem(new GUIContent("Occluder Static"), (intStaticFlags & (int)StaticEditorFlags.OccluderStatic) > 0, staticChangeHandler, (int)StaticEditorFlags.OccluderStatic);
+                menu.AddItem(new GUIContent("Batching Static"), (intStaticFlags & (int)StaticEditorFlags.BatchingStatic) > 0, staticChangeHandler, (int)StaticEditorFlags.BatchingStatic);
+                menu.AddItem(new GUIContent("Navigation Static"), (intStaticFlags & (int)StaticEditorFlags.NavigationStatic) > 0, staticChangeHandler, (int)StaticEditorFlags.NavigationStatic);
+                menu.AddItem(new GUIContent("Occludee Static"), (intStaticFlags & (int)StaticEditorFlags.OccludeeStatic) > 0, staticChangeHandler, (int)StaticEditorFlags.OccludeeStatic);
+                menu.AddItem(new GUIContent("Off Mesh Link Generation"), (intStaticFlags & (int)StaticEditorFlags.OffMeshLinkGeneration) > 0, staticChangeHandler,
+                    (int)StaticEditorFlags.OffMeshLinkGeneration);
+#if UNITY_4_6 || UNITY_4_7
+#else
+                menu.AddItem(new GUIContent("Reflection Probe Static"), (intStaticFlags & (int)StaticEditorFlags.ReflectionProbeStatic) > 0, staticChangeHandler,
+                    (int)StaticEditorFlags.ReflectionProbeStatic);
+#endif
                 menu.ShowAsContext();
             }
         }
@@ -124,7 +127,7 @@ namespace qtools.qhierarchy.pcomponent
             for (int i = gameObjects.Length - 1; i >= 0; i--)
             {
                 GameObject gameObject = gameObjects[i];
-                Undo.RecordObject(gameObject, "Change Static Flags");            
+                Undo.RecordObject(gameObject, "Change Static Flags");
                 GameObjectUtility.SetStaticEditorFlags(gameObject, resultStaticFlags);
                 EditorUtility.SetDirty(gameObject);
             }
@@ -147,4 +150,3 @@ namespace qtools.qhierarchy.pcomponent
         }
     }
 }
-
