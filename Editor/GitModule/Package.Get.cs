fb27ba7b11fe2930f75b54d4b5eede603c26b948
sam <1398581458@qq.com>
2023-07-24 12:06:40 +0800

    chiyu sam 自动生成拉取代码

---
+++ Editor/GitModule/Package.Get.cs
@@ -0,0 +1,81 @@
+﻿/*|✩ - - - - - |||
+|||✩ Author:   ||| -> SAM
+|||✩ Date:     ||| -> 2023-07-24
+|||✩ Document: ||| -> 
+|||✩ - - - - - |*/
+
+using System.Text.RegularExpressions;
+using AIO;
+using UnityEditor;
+using UnityEngine;
+
+namespace AIO.Unity.Editor
+{
+    /// <summary>
+    /// Package
+    /// </summary>
+    public partial class Package
+    {
+        internal static string URL { get; }
+        internal const string Label = "Package";
+        internal const string RegexName = "com.*.package";
+
+        static Package()
+        {
+            var regex = new Regex(RegexName);
+            foreach (var info in UtilsGen.IO.GetFoldersInfo(Application.dataPath.Replace("Assets", "Packages")))
+                if (regex.IsMatch(info.Name))
+                    URL = info.FullName;
+        }
+
+        internal const string Command_Git_Pull = "Git Pull";
+        internal const string Command_Git_Push = "Git Push";
+        internal const string Command_Git_Add = "Git Add";
+        internal const string Command_Git_Commit = "Git Commit";
+        internal const string Command_Git_Upload = "Git Upload";
+        internal const string Command_Git_Remote_Pull = "Git Remote Pull";
+        internal const string Command_Git_Clean_FDX = "Git Clean";
+
+        [MenuItem("Package/" + Command_Git_Remote_Pull + "/" + Label)]
+        public static void GitRemoteSetUrl()
+        {
+            PrPlatform.Git.RemoteSetUrl(URL, false).Async();
+        }
+
+        [MenuItem("Package/" + Command_Git_Clean_FDX + "/" + Label)]
+        public static async void GitClean()
+        {
+            await PrGit.Clean.ForceDirectoryx(URL).Async();
+        }
+
+        [MenuItem("Package/" + Command_Git_Pull + "/" + Label)]
+        public static void GitPull()
+        {
+            PrPlatform.Git.Pull(URL, false).Async();
+        }
+
+        [MenuItem("Package/" + Command_Git_Commit + "/" + Label)]
+        public static void GitCommit()
+        {
+            PrPlatform.Git.Commit((URL, null), false).Async();
+        }
+
+        [MenuItem("Package/" + Command_Git_Upload + "/" + Label)]
+        public static void GitUpload()
+        {
+            PrPlatform.Git.Upload(URL, false, false, false).Async();
+        }
+
+        [MenuItem("Package/" + Command_Git_Add + "/" + Label)]
+        public static void GitAdd()
+        {
+            PrPlatform.Git.Add(URL, false).Async();
+        }
+
+        [MenuItem("Package/" + Command_Git_Push + "/" + Label)]
+        public static void GitPush()
+        {
+            PrPlatform.Git.Push((URL, null), false).Async();
+        }
+    }
+}
---
+++ Editor/GitModule/PackageGen.cs
@@ -0,0 +1,102 @@
+﻿/*|✩ - - - - - |||
+|||✩ Author:   ||| -> SAM
+|||✩ Date:     ||| -> 2023-07-24
+|||✩ Document: ||| -> 
+|||✩ - - - - - |*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+namespace AIO.Unity.Editor
+{
+    /// <summary>
+    /// PackageGen
+    /// </summary>
+    internal class PackageGen
+    {
+        /// <summary>
+        /// 生成
+        /// </summary>
+        [MenuItem("Package/~~~测试~~~/生成")]
+        internal static void Generate()
+        {
+            var search = Client.Search("com.blz.package");
+            var packageInfos = AssetDatabase.FindAssets("package", new string[] { "Packages" })
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Where(x => AssetDatabase.LoadAssetAtPath<TextAsset>(x) != null)
+                .Select(PackageInfo.FindForAssetPath)
+                .GroupBy(x => x.assetPath)
+                .Select(y => y.First())
+                .Where(x => Directory.Exists(Path.Combine(x.assetPath, ".git")))
+                .ToList();
+            CreateTemplate(packageInfos);
+        }
+
+        private const string CMD_Git = "Git";
+        internal const string CMD_Git_Pull = "Pull";
+        internal const string CMD_Git_Push = "Push";
+        internal const string CMD_Git_Add = "Add";
+        internal const string CMD_Git_Commit = "Commit";
+        internal const string CMD_Git_Upload = "Upload";
+        internal const string CMD_Git_Remote_Pull = "Remote Pull";
+        internal const string CMD_Git_Clean_FDX = "Clean";
+
+        public static void CreateTemplate(IEnumerable<PackageInfo> infos)
+        {
+            var OutPath = Application.dataPath.Replace("Assets", "Packages/com.blz.package/Editor/GitModule/AutomaticGeneration");
+
+            const string tips = @"/*|============================================|*|
+|*|Author:        |*|Automatic Generation      |*|
+|*|Date:          |*|{0}                |*|
+|*|=============================================*/";
+
+            const string usings = @"
+    using UnityEditor;
+    using UnityEngine;
+";
+
+            var savaDir = new Dictionary<string, string>();
+            var str = new StringBuilder();
+
+            foreach (var info in infos)
+            {
+                str.Clear();
+                str.AppendFormat(tips, DateTime.Now.ToString("yyyy-MM-dd")).Append("\r\n\r\n");
+                str.AppendFormat("namespace {0}\r\n{1}", "AIO.Unity.Editor", "{");
+                str.AppendFormat("{0}\r\n", usings);
+
+                str.AppendFormat("    /// <summary>\r\n    /// Git管理 {0}\r\n    /// </summary>\r\n", info.displayName);
+                str.AppendFormat("    internal static partial class Git_{0}\r\n", info.name.Replace('.', '_').ToUpper()).Append("    {\r\n");
+
+                str.AppendFormat("        internal const string URL = \"{0}\";\r\n", info.assetPath);
+                str.AppendFormat("        internal const string DisplayName = \"{0}\";\r\n", info.displayName);
+                str.AppendFormat("        internal const string PackageName = \"{0}\";\r\n", info.name);
+
+                str.AppendFormat("        [MenuItem(\"{0}/{1}/\" + DisplayName)]\r\n",
+                    CMD_Git, CMD_Git_Add);
+                str.AppendFormat("        internal static async void {0}()\r\n{1}",
+                    nameof(CMD_Git_Add), "        {\r\n");
+                str.AppendFormat("            await PrPlatform.Git.{0}(Application.dataPath.Replace(\"Assets\", URL), false).Async();\r\n{1}",
+                    CMD_Git_Add, "        }\r\n");
+
+                str.Append("\r\n    }\r\n}");
+                savaDir.Add(info.name, str.ToString());
+            }
+
+            var bakdir = new DirectoryInfo(OutPath);
+            if (bakdir.Exists) Directory.Delete(OutPath, true);
+            bakdir.Create();
+
+            foreach (var item in savaDir)
+                File.WriteAllText(Path.Combine(OutPath, string.Concat(item.Key, ".cs")), item.Value, Encoding.UTF8);
+        }
+    }
+}
