766e5e1d1f736f704b15aa99180752f9bc452743
sam <1398581458@qq.com>
2023-08-03 11:57:40 +0800

    chiyu sam 更新yooasset过滤

--- Editor/Extensions/Script/YooAsset/IAddressRule/AddressRuleRelativeCollect.cs
+++ Editor/Extensions/Script/YooAsset/IAddressRule/AddressRuleRelativeCollect.cs
@@ -13,6 +13,24 @@ namespace AIO.Unity.Editor
         }
     }
 
+    [DisplayName("Collect + 后缀 = 定位地址  (全小写)")]
+    public class AddressRuleRelativeCollectWithSuffixToLower : IAddressRule
+    {
+        string IAddressRule.GetAssetAddress(AddressRuleData data)
+        {
+            return data.AssetPath.Replace(data.CollectPath + "/", "").ToLower();
+        }
+    }
+
+    [DisplayName("Collect + 后缀 = 定位地址  (全大写)")]
+    public class AddressRuleRelativeCollectWithSuffixToUpper : IAddressRule
+    {
+        string IAddressRule.GetAssetAddress(AddressRuleData data)
+        {
+            return data.AssetPath.Replace(data.CollectPath + "/", "").ToUpper();
+        }
+    }
+
     [DisplayName("Collect = 定位地址")]
     public class AddressRuleRelativeCollectNoSuffix : IAddressRule
     {
@@ -22,6 +40,30 @@ namespace AIO.Unity.Editor
             var suffix = Path.GetExtension(path);
             if (string.IsNullOrEmpty(suffix)) return path;
             return path.Replace(Path.GetExtension(path), "");
+        }
+    }
+
+    [DisplayName("Collect = 定位地址 (全小写)")]
+    public class AddressRuleRelativeCollectNoSuffixToLower : IAddressRule
+    {
+        string IAddressRule.GetAssetAddress(AddressRuleData data)
+        {
+            var path = data.AssetPath.Replace(string.Concat(data.CollectPath, "/"), "");
+            var suffix = Path.GetExtension(path);
+            if (string.IsNullOrEmpty(suffix)) return path;
+            return path.Replace(Path.GetExtension(path), "").ToLower();
+        }
+    }
+
+    [DisplayName("Collect = 定位地址  (全大写)")]
+    public class AddressRuleRelativeCollectNoSuffixToUpper : IAddressRule
+    {
+        string IAddressRule.GetAssetAddress(AddressRuleData data)
+        {
+            var path = data.AssetPath.Replace(string.Concat(data.CollectPath, "/"), "");
+            var suffix = Path.GetExtension(path);
+            if (string.IsNullOrEmpty(suffix)) return path;
+            return path.Replace(Path.GetExtension(path), "").ToUpper();
         }
     }
 
--- Editor/Extensions/Script/YooAsset/IFilterRule/Filter/FilterRulePrefab.cs
+++ Editor/Extensions/Script/YooAsset/IFilterRule/Filter/FilterRulePrefab.cs
@@ -18,5 +18,48 @@ namespace AIO.Unity.Editor
             return Path.GetExtension(data.AssetPath).ToLower() != ".prefab";
         }
     }
+
+    [DisplayName("过滤 SpriteAtlas")]
+    public class FilterRuleSpriteAtlas : IFilterRule
+    {
+        public bool IsCollectAsset(FilterRuleData data)
+        {
+            return Path.GetExtension(data.AssetPath).ToLower() != ".spriteatlas";
+        }
+    }
+
+    [DisplayName("过滤 SpriteAtlas Prefab")]
+    public class FilterRuleSpriteAtlasPrefab : IFilterRule
+    {
+        public bool IsCollectAsset(FilterRuleData data)
+        {
+            var Extension = Path.GetExtension(data.AssetPath).ToLower();
+            return Extension != ".spriteatlas" &&
+                   Extension != ".prefab";
+        }
+    }
+    
+    [DisplayName("过滤 Scene Prefab")]
+    public class FilterRuleScenePrefab : IFilterRule
+    {
+        public bool IsCollectAsset(FilterRuleData data)
+        {
+            var Extension = Path.GetExtension(data.AssetPath).ToLower();
+            return Extension != ".unity" &&
+                   Extension != ".prefab";
+        }
+    }
+
+    [DisplayName("过滤 SpriteAtlas Prefab Scene")]
+    public class FilterRuleSpriteAtlasPrefabScene : IFilterRule
+    {
+        public bool IsCollectAsset(FilterRuleData data)
+        {
+            var Extension = Path.GetExtension(data.AssetPath).ToLower();
+            return Extension != ".spriteatlas" &&
+                   Extension != ".prefab" &&
+                   Extension != ".unity";
+        }
+    }
 }
 #endif
