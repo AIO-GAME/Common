1b738d0e71fc7f3ebfbff20be4134d56ec9ff814
sam <1398581458@qq.com>
2023-08-07 14:51:18 +0800

    chiyu sam 更新

---
+++ Editor/Other/ToolKit/Importer/SprotoImport.cs
@@ -0,0 +1,28 @@
+﻿/*|✩ - - - - - |||
+|||✩ Author:   ||| -> SAM
+|||✩ Date:     ||| -> 2023-08-07
+|||✩ Document: ||| -> 
+|||✩ - - - - - |*/
+
+using System.IO;
+using UnityEngine;
+#if UNITY_2019_1_OR_NEWER
+using UnityEditor.AssetImporters;
+#endif
+
+#if UNITY_2018_1_OR_NEWER && !UNITY_2020_1_OR_NEWER
+using UnityEditor.Experimental.AssetImporters;
+#endif
+namespace AIO.UEditor
+{
+    [ScriptedImporter(0, "sproto")]
+    public class SprotoImporter : ScriptedImporter
+    {
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            var asset = new TextAsset(File.ReadAllText(ctx.assetPath));
+            ctx.AddObjectToAsset("main obj", asset);
+            ctx.SetMainObject(asset);
+        }
+    }
+}
