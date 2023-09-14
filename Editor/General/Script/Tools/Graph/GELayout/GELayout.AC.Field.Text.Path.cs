ba02c35c7db279f144b433f7d4d5b2d6279ceedf
Xinansky <1398581458@qq.com>
2023-09-14 14:17:40 +0800

    [change] 更新

--- Editor/General/Script/Tools/Graph/GELayout/GELayout.AC.Field.Text.Path.cs
+++ Editor/General/Script/Tools/Graph/GELayout/GELayout.AC.Field.Text.Path.cs
@@ -11,13 +11,17 @@ namespace AIO.UEditor
 {
     public partial class GELayout
     {
-        public static string Path(string label, string value, string tips = "Please select the path", string defaultName = "")
+        public static string Path(string label, string value, string tips = "Please select the path",
+            string defaultName = "")
         {
             Horizontal(() =>
             {
                 Label(label);
                 if (Button("Select", GUILayout.Width(50)))
                     value = EditorUtility.OpenFolderPanel(tips, value, defaultName);
+                if (string.IsNullOrEmpty(value)) return;
+                if (Button("Open", GUILayout.Width(50)))
+                    PrPlatform.Open.Path(value).Async();
             });
             return value;
         }
