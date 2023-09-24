621a64bf455849e8934e9cd4c4392fb32a961b34
XiNanSky <1398581458@qq.com>
2023-09-24 22:58:31 +0800

    [update] 更新

---
+++ Runtime/Tools/Graph/GULayout/GULayout.AC.cs
@@ -0,0 +1,49 @@
+﻿/*|✩ - - - - - |||
+|||✩ Author:   ||| -> Sam Li
+|||✩ Date:     ||| -> 2023-06-29
+|||✩ Document: ||| ->
+|||✩ - - - - - |*/
+
+using UnityEngine;
+
+namespace AIO
+{
+    public partial class GULayout
+    {
+        /// <summary>
+        /// 复制字符按钮
+        /// </summary>
+        public static void ButtonCopyText(string name, string content, GUIStyle style, float height = 30, float width = 100)
+        {
+            if (GUILayout.Button(name, style, GUILayout.Height(height), GUILayout.Width(width)))
+                GUHelper.CopyTextAction(content);
+        }
+
+        /// <summary>
+        /// 复制字符按钮
+        /// </summary>
+        public static void ButtonCopyText(string name, string content, float height = 30, float width = 100)
+        {
+            if (GUILayout.Button(name, GUILayout.Height(height), GUILayout.Width(width)))
+                GUHelper.CopyTextAction(content);
+        }
+        
+        /// <summary>
+        /// 复制字符按钮
+        /// </summary>
+        public static void ButtonCopyText(GUIContent name, string content, GUIStyle style, float height = 30, float width = 100)
+        {
+            if (GUILayout.Button(name, style, GUILayout.Height(height), GUILayout.Width(width)))
+                GUHelper.CopyTextAction(content);
+        }
+
+        /// <summary>
+        /// 复制字符按钮
+        /// </summary>
+        public static void ButtonCopyText(GUIContent name, string content, float height = 30, float width = 100)
+        {
+            if (GUILayout.Button(name, GUILayout.Height(height), GUILayout.Width(width)))
+                GUHelper.CopyTextAction(content);
+        }
+    }
+}
