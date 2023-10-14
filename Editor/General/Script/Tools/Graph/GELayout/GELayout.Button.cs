af8528b21b8c9891a5aa9c3b6852d286fa845c3c
XiNanSky <1398581458@qq.com>
2023-10-14 13:09:51 +0800

    [update] GELayout.Selection

--- Editor/General/Script/Tools/Graph/GELayout/GELayout.Button.cs
+++ Editor/General/Script/Tools/Graph/GELayout/GELayout.Button.cs
@@ -4,14 +4,63 @@
 |*|E-Mail:     |*| 1398581458@qq.com     
 |*|============|*/
 
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace AIO.UEditor
 {
     public partial class GELayout
-    {  
-       
+    {
+        /// <summary>
+        /// Make a Selection Grid 
+        /// </summary>
+        /// <param name="selected">The index of the selected button. <see cref="int"/></param>
+        /// <param name="contents">An array of text, image and tooltips for the button.</param>
+        /// <param name="cb">回调</param>
+        /// <param name="width">单体宽度</param>
+        /// <returns><see cref="T"/>选中目标</returns>
+        public static T Selection<T>(T selected, IDictionary<string, T> contents, Action<T> cb, float width = 100)
+        {
+            using (new GUILayout.HorizontalScope(GEStyle.PRInsertion, GUILayout.Width(contents.Count * width)))
+            {
+                foreach (var content in contents)
+                {
+                    if (GUILayout.Button(content.Key, selected.Equals(content.Value) ? GEStyle.PreLabel : GEStyle.INEditColliderButton, GUILayout.Width(width)))
+                    {
+                        selected = content.Value;
+                        cb(selected);
+                        return selected;
+                    }
+                }
+            }
 
+            return selected;
+        }
+        /// <summary>
+        /// Make a Selection Grid 
+        /// </summary>
+        /// <param name="selected">The index of the selected button. <see cref="int"/></param>
+        /// <param name="contents">An array of text, image and tooltips for the button.</param>
+        /// <param name="cb">回调</param>
+        /// <param name="width">单体宽度</param>
+        /// <returns><see cref="T"/>选中目标</returns>
+        public static T Selection<T>(T selected, IDictionary<GUIContent, T> contents, Action<T> cb, float width = 100)
+        {
+            using (new GUILayout.HorizontalScope(GEStyle.PRInsertion, GUILayout.Width(contents.Count * width)))
+            {
+                foreach (var content in contents)
+                {
+                    if (GUILayout.Button(content.Key, selected.Equals(content.Value) ? GEStyle.PreLabel : GEStyle.INEditColliderButton, GUILayout.Width(width)))
+                    {
+                        selected = content.Value;
+                        cb(selected);
+                        return selected;
+                    }
+                }
+            }
+
+            return selected;
+        }
     }
 }
--- Editor/General/Script/Tools/Graph/GEStyle/U.GUIStyle.cs
+++ Editor/General/Script/Tools/Graph/GEStyle/U.GUIStyle.cs
@@ -28,18 +28,109 @@ namespace AIO.UEditor
         /// <summary>
         ///
         /// </summary>
+        public static UGUIStyle PRInsertion => Get("PR Insertion");
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle TVInsertion => Get("TV Insertion");
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle TEtoolbarbutton => Get("TE toolbarbutton");
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle toolbarbutton => Get("toolbarbutton");
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle ToolbarDropDownLeft => Get(nameof(ToolbarDropDownLeft));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle RectangleToolScaleBottom => Get(nameof(RectangleToolScaleBottom));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle RectangleToolScaleRight => Get(nameof(RectangleToolScaleRight));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle sv_label_0 => Get(nameof(sv_label_0));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle sv_label_1 => Get(nameof(sv_label_1));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle sv_label_2 => Get(nameof(sv_label_2));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle sv_label_3 => Get(nameof(sv_label_3));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle sv_label_4 => Get(nameof(sv_label_4));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle sv_label_5 => Get(nameof(sv_label_5));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle sv_label_6 => Get(nameof(sv_label_6));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle sv_label_7 => Get(nameof(sv_label_7));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle RectangleToolScaleLeft => Get(nameof(RectangleToolScaleLeft));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle RectangleToolScaleTop => Get(nameof(RectangleToolScaleTop));
+
+
+        /// <summary>
+        ///
+        /// </summary>
         public static UGUIStyle ObjectPickerLargeStatus => Get("ObjectPickerLargeStatus");
 
         /// <summary>
         ///
         /// </summary>
         public static UGUIStyle WhiteLargeCenterLabel => Get("WhiteLargeCenterLabel");
-        
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle flowvarPintooltip => Get("flow varPin tooltip");
+
         /// <summary>
         ///
         /// </summary>
         public static UGUIStyle LargeButton => Get(nameof(LargeButton));
-        
+
         /// <summary>
         ///
         /// </summary>
@@ -58,7 +149,27 @@ namespace AIO.UEditor
         /// <summary>
         ///
         /// </summary>
+        public static UGUIStyle RLHeader => Get("RL Header");
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle RLEmptyFooter => Get("RL Empty Header");
+
+        /// <summary>
+        ///
+        /// </summary>
         public static UGUIStyle ChannelStripBg => Get(nameof(ChannelStripBg));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle ChannelStripAttenuationBar => Get(nameof(ChannelStripAttenuationBar));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle ChannelStripAttenuationMarkerSquare => Get(nameof(ChannelStripAttenuationMarkerSquare));
 
         /// <summary>
         ///
@@ -69,6 +180,11 @@ namespace AIO.UEditor
         ///
         /// </summary>
         public static UGUIStyle INThumbnailShadow => Get("IN ThumbnailShadow");
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle GroupBox => Get("GroupBox");
 
         /// <summary>
         ///
@@ -128,26 +244,76 @@ namespace AIO.UEditor
         /// <summary>
         ///
         /// </summary>
+        public static UGUIStyle ProjectBrowserTextureIconDropShadow => Get(nameof(ProjectBrowserTextureIconDropShadow));
+
+        /// <summary>
+        ///
+        /// </summary>
         public static UGUIStyle DDHeaderStyle => Get("DD HeaderStyle");
 
         /// <summary>
         ///
         /// </summary>
-        public static UGUIStyle ContentToolbar => Get("ContentToolbar");
+        public static UGUIStyle DDItemCheckmark => Get("DD ItemCheckmark");
 
         /// <summary>
         ///
         /// </summary>
-        public static UGUIStyle HelpBox => Get("HelpBox");
+        public static UGUIStyle INEditColliderButton => Get("IN EditColliderButton");
 
         /// <summary>
         ///
         /// </summary>
-        public static UGUIStyle CenteredLabel => Get("CenteredLabel");
+        public static UGUIStyle dockareaOverlay => Get(nameof(dockareaOverlay));
 
         /// <summary>
         ///
         /// </summary>
-        public static UGUIStyle DropzoneStyle => Get("DropzoneStyle");
+        public static UGUIStyle ContentToolbar => Get(nameof(ContentToolbar));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle HelpBox => Get(nameof(HelpBox));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle ShurikenModuleTitle => Get(nameof(ShurikenModuleTitle));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle NotificationBackground => Get(nameof(NotificationBackground));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle CenteredLabel => Get(nameof(CenteredLabel));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle PreLabel => Get(nameof(PreLabel));
+        
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle TEDefaultTime => Get("TE DefaultTime");
+        
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle hostview => Get(nameof(hostview));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle HeaderButton => Get(nameof(HeaderButton));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static UGUIStyle DropzoneStyle => Get(nameof(DropzoneStyle));
     }
 }
