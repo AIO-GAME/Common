20fbf23dee70cb28df8513a6b3db77b0d39b341d
Xinansky <1398581458@qq.com>
2023-10-17 18:03:33 +0800

    [move] 移动git插件库

--- Editor/Helper/MenuItem/Git/MenuItem.Git.ALL.cs
+++ Editor/Helper/MenuItem/Git/MenuItem.Git.ALL.cs
@@ -1,166 +1,166 @@
-﻿/*|✩ - - - - - |||
-|||✩ Author:   ||| -> XINAN
-|||✩ Date:     ||| -> 2023-07-24
-|||✩ Document: ||| ->
-|||✩ - - - - - |*/
-
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Newtonsoft.Json;
-using UnityEditor;
-using UnityEngine;
-using PackageInfo = UnityEditor.PackageManager.PackageInfo;
-
-namespace AIO.UEditor
-{
-    internal static partial class PackageGen
-    {
-        private static IEnumerable<PackageInfo> GetInfo()
-        {
-            var packageInfos = AssetDatabase.FindAssets("package", new string[] { "Packages" })
-                .Select(AssetDatabase.GUIDToAssetPath)
-                .Where(x => AssetDatabase.LoadAssetAtPath<TextAsset>(x) != null)
-                .Select(PackageInfo.FindForAssetPath)
-                .GroupBy(x => x.assetPath)
-                .Select(x => x.First())
-                .Where(x => AHelper.IO.Exists(Path.Combine(x.resolvedPath, ".git")))
-                .ToList();
-
-            return packageInfos;
-        }
-
-        #region Git All
-
-        [MenuItem(CMD_GIT + "/All/克隆 Clone", false, 0)]
-        private static async void GITClone()
-        {
-            var PackagePath = Application.dataPath.Replace("Assets", "Packages");
-            var FilePath = Path.Combine(PackagePath, "AutoGitClone.ini");
-            var PackageData = JsonConvert.DeserializeObject<Hashtable>(File.ReadAllText(FilePath));
-            await PrPlatform.Git.Clone(PackagePath, PackageData.Get<List<string>>("URL"), false);
-        }
-
-        [MenuItem(CMD_GIT + "/All/添加 Add [.]", false, 1)]
-        private static async void GITAdd()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrPlatform.Git.Add(list, ".", false);
-        }
-
-        [MenuItem(CMD_GIT + "/All/拉取 Pull", false, 2)]
-        private static async void GITPull()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrPlatform.Git.Pull(list, false);
-        }
-
-#if UNITY_EDITOR_WIN
-        [MenuItem(CMD_GIT + "/All/拉取分支 Pull Branch", false, 2)]
-        private static async void GITPullBranch()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrWin.Git.PullBranch(list, false);
-        }
-#endif
-
-        [MenuItem(CMD_GIT + "/All/推送 Push", false, 3)]
-        private static async void GITPush()
-        {
-            var list = GetInfo()
-                .Select(x => (x.resolvedPath, ""))
-                .ToList();
-            await PrPlatform.Git.Push(list, false);
-        }
-
-        [MenuItem(CMD_GIT + "/All/提交 Commit", false, 4)]
-        private static async void GITCommit()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrPlatform.Git.Commit(list, false);
-        }
-
-        [MenuItem(CMD_GIT + "/All/上传 Pull Add Commit Push", false, 5)]
-        private static async void GITUpload()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrPlatform.Git.Upload(list, true, false, false);
-        }
-
-        [MenuItem(CMD_GIT + "/All/清理 Clean/-FDX 强制清理文件夹 不受忽略文件影响", false, 6)]
-        private static async void CleanFDX()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrPlatform.Git.Clean(list, "-fdx", false);
-        }
-
-        [MenuItem(CMD_GIT + "/All/清理 Clean/-FD 强制清理文件夹", false, 6)]
-        private static async void CleanFD()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrPlatform.Git.Clean(list, "-fd", false);
-        }
-
-        [MenuItem(CMD_GIT + "/All/重置 Reset/--Hard 重置 [分支 暂存区 工作区]", false, 6)]
-        private static async void ResetHard()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrPlatform.Git.ResetHard(list, false);
-        }
-
-        [MenuItem(CMD_GIT + "/All/重置 Reset/--Keep 重置 [索引] 如果提交和HEAD之间的文件与HEAD不同，则重置将中止", false, 6)]
-        private static async void ResetKeep()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrPlatform.Git.ResetKeep(list, false);
-        }
-
-        [MenuItem(CMD_GIT + "/All/重置 Reset/--Merge 重置 [索引 暂存区] 更改和索引产生 重置将被终止", false, 6)]
-        private static async void ResetMerge()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrPlatform.Git.ResetMerge(list, false);
-        }
-
-        [MenuItem(CMD_GIT + "/All/重置 Reset/--Mixed 重置 [分支 暂存区]", false, 6)]
-        private static async void ResetMixed()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrPlatform.Git.ResetMixed(list, false);
-        }
-
-        [MenuItem(CMD_GIT + "/All/重置 Reset/--Soft 重置 [分支]", false, 6)]
-        private static async void ResetSoft()
-        {
-            var list = GetInfo()
-                .Select(x => x.resolvedPath)
-                .ToList();
-            await PrPlatform.Git.ResetSoft(list, false);
-        }
-
-        #endregion
-    }
-}
+﻿// /*|✩ - - - - - |||
+// |||✩ Author:   ||| -> XINAN
+// |||✩ Date:     ||| -> 2023-07-24
+// |||✩ Document: ||| ->
+// |||✩ - - - - - |*/
+//
+// using System.Collections;
+// using System.Collections.Generic;
+// using System.IO;
+// using System.Linq;
+// using Newtonsoft.Json;
+// using UnityEditor;
+// using UnityEngine;
+// using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+//
+// namespace AIO.UEditor
+// {
+//     internal static partial class PackageGen
+//     {
+//         private static IEnumerable<PackageInfo> GetInfo()
+//         {
+//             var packageInfos = AssetDatabase.FindAssets("package", new string[] { "Packages" })
+//                 .Select(AssetDatabase.GUIDToAssetPath)
+//                 .Where(x => AssetDatabase.LoadAssetAtPath<TextAsset>(x) != null)
+//                 .Select(PackageInfo.FindForAssetPath)
+//                 .GroupBy(x => x.assetPath)
+//                 .Select(x => x.First())
+//                 .Where(x => AHelper.IO.Exists(Path.Combine(x.resolvedPath, ".git")))
+//                 .ToList();
+//
+//             return packageInfos;
+//         }
+//
+//         #region Git All
+//
+//         [MenuItem(CMD_GIT + "/All/克隆 Clone", false, 0)]
+//         private static async void GITClone()
+//         {
+//             var PackagePath = Application.dataPath.Replace("Assets", "Packages");
+//             var FilePath = Path.Combine(PackagePath, "AutoGitClone.ini");
+//             var PackageData = JsonConvert.DeserializeObject<Hashtable>(File.ReadAllText(FilePath));
+//             await PrPlatform.Git.Clone(PackagePath, PackageData.Get<List<string>>("URL"), false);
+//         }
+//
+//         [MenuItem(CMD_GIT + "/All/添加 Add [.]", false, 1)]
+//         private static async void GITAdd()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrPlatform.Git.Add(list, ".", false);
+//         }
+//
+//         [MenuItem(CMD_GIT + "/All/拉取 Pull", false, 2)]
+//         private static async void GITPull()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrPlatform.Git.Pull(list, false);
+//         }
+//
+// #if UNITY_EDITOR_WIN
+//         [MenuItem(CMD_GIT + "/All/拉取分支 Pull Branch", false, 2)]
+//         private static async void GITPullBranch()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrWin.Git.PullBranch(list, false);
+//         }
+// #endif
+//
+//         [MenuItem(CMD_GIT + "/All/推送 Push", false, 3)]
+//         private static async void GITPush()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => (x.resolvedPath, ""))
+//                 .ToList();
+//             await PrPlatform.Git.Push(list, false);
+//         }
+//
+//         [MenuItem(CMD_GIT + "/All/提交 Commit", false, 4)]
+//         private static async void GITCommit()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrPlatform.Git.Commit(list, false);
+//         }
+//
+//         [MenuItem(CMD_GIT + "/All/上传 Pull Add Commit Push", false, 5)]
+//         private static async void GITUpload()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrPlatform.Git.Upload(list, true, false, false);
+//         }
+//
+//         [MenuItem(CMD_GIT + "/All/清理 Clean/-FDX 强制清理文件夹 不受忽略文件影响", false, 6)]
+//         private static async void CleanFDX()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrPlatform.Git.Clean(list, "-fdx", false);
+//         }
+//
+//         [MenuItem(CMD_GIT + "/All/清理 Clean/-FD 强制清理文件夹", false, 6)]
+//         private static async void CleanFD()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrPlatform.Git.Clean(list, "-fd", false);
+//         }
+//
+//         [MenuItem(CMD_GIT + "/All/重置 Reset/--Hard 重置 [分支 暂存区 工作区]", false, 6)]
+//         private static async void ResetHard()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrPlatform.Git.ResetHard(list, false);
+//         }
+//
+//         [MenuItem(CMD_GIT + "/All/重置 Reset/--Keep 重置 [索引] 如果提交和HEAD之间的文件与HEAD不同，则重置将中止", false, 6)]
+//         private static async void ResetKeep()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrPlatform.Git.ResetKeep(list, false);
+//         }
+//
+//         [MenuItem(CMD_GIT + "/All/重置 Reset/--Merge 重置 [索引 暂存区] 更改和索引产生 重置将被终止", false, 6)]
+//         private static async void ResetMerge()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrPlatform.Git.ResetMerge(list, false);
+//         }
+//
+//         [MenuItem(CMD_GIT + "/All/重置 Reset/--Mixed 重置 [分支 暂存区]", false, 6)]
+//         private static async void ResetMixed()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrPlatform.Git.ResetMixed(list, false);
+//         }
+//
+//         [MenuItem(CMD_GIT + "/All/重置 Reset/--Soft 重置 [分支]", false, 6)]
+//         private static async void ResetSoft()
+//         {
+//             var list = GetInfo()
+//                 .Select(x => x.resolvedPath)
+//                 .ToList();
+//             await PrPlatform.Git.ResetSoft(list, false);
+//         }
+//
+//         #endregion
+//     }
+// }
--- Tools~/Process/Process/Script/Platform/PrPlatform.Git.Pull.cs
+++ Tools~/Process/Process/Script/Platform/PrPlatform.Git.Pull.cs
@@ -21,6 +21,29 @@ namespace AIO
             /// <param name="quit">静默退出</param>
             /// <exception cref="NotImplementedException">未实现</exception>
             /// <returns><see cref="IExecutor"/>执行器</returns>
+            public static IExecutor PullBranch(ICollection<string> targets, bool quit = false)
+            {
+                switch (Environment.OSVersion.Platform)
+                {
+                    case PlatformID.Win32NT:
+                    case PlatformID.Win32S:
+                    case PlatformID.Win32Windows:
+                    case PlatformID.WinCE:
+                        return PrWin.Git.PullBranch(targets, quit);
+                    case PlatformID.MacOSX:
+                    case PlatformID.Unix:
+                        return PrMac.Git.PullBranch(targets, quit);
+                    default: throw new NotImplementedException();
+                }
+            }
+
+            /// <summary>
+            /// <see cref="PrPlatform"/> Git Pull
+            /// </summary>
+            /// <param name="targets">目标路径</param>
+            /// <param name="quit">静默退出</param>
+            /// <exception cref="NotImplementedException">未实现</exception>
+            /// <returns><see cref="IExecutor"/>执行器</returns>
             public static IExecutor Pull(ICollection<string> targets, bool quit = false)
             {
                 switch (Environment.OSVersion.Platform)
