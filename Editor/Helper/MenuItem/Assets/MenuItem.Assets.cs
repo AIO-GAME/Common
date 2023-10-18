01cbf79118d8326f95321d95c70e77d80a600e05
Xinansky <1398581458@qq.com>
2023-10-18 17:28:02 +0800

    屏蔽

--- Editor/Helper/MenuItem/Assets/MenuItem.Assets.cs
+++ Editor/Helper/MenuItem/Assets/MenuItem.Assets.cs
@@ -13,17 +13,17 @@ namespace AIO.UEditor
     public static partial class EditorMenu_Assets
     {
 #if UNITY_EDITOR_WIN
-        [MenuItem("Assets/Open C# Project DLL")]
+        [MenuItem("Assets/Open C# Project AIO DLL")]
         public static async void OpenDllProjectWIN()
         {
             var info = PackageInfo.FindForAssembly(typeof(EditorMenu_Assets).Assembly);
-            var executor = PrCmd.Create().Input($"start \"DLL Project\" {Path.Combine(info.resolvedPath, "Tools~", "ALL.sln")} /B /Max /HIGH");
+            var executor = PrCmd.Create().Input($"start \"AIO DLL Project\" {Path.Combine(info.resolvedPath, "Tools~", "ALL.sln")} /B /Max /HIGH");
             (await executor.Async()).Debug();
         }
 #endif
 
 #if UNITY_EDITOR_OSX
-        [MenuItem("Assets/Open C# Project DLL")]
+        [MenuItem("Assets/Open C# Project AIO DLL")]
         public static async void OpenDllProjectOSX()
         {
             var info = PackageInfo.FindForAssembly(typeof(EditorMenu_Assets).Assembly);
