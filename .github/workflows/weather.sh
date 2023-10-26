68569ab842161fda22861eebbf1d5a643c155425
XiNanSky <1398581458@qq.com>
2023-10-26 23:44:58 +0800

    ⭐[remove] workflows

--- .github/workflows/weather.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-set -eux
-curl -H "Accept-Language: zh-CN" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36" -o result.html https://wttr.in/Shenzhen
