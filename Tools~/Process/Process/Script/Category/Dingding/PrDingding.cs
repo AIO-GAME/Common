8145c01a58944c6879afd0d4a8fd649591005673
xinansky <1398581458@qq.com>
2023-11-14 10:26:00 +0800

    ⭐[update] 新增钉钉机器人

--- Tools~/Process/Process/Script/Category/Dingding/PrDingding.cs
+++ Tools~/Process/Process/Script/Category/Dingding/PrDingding.cs
@@ -4,6 +4,7 @@
 |*|E-Mail:        |*|1398581458@qq.com         |*|
 |*|=============================================*/
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
@@ -27,15 +28,9 @@ namespace AIO
         /// <summary>
         /// 消息
         /// </summary>
-        public static Msg CreateMSG(MsgType type = MsgType.empty)
+        public static Msg CreateData()
         {
-            var data = new Msg
-            {
-                Type = type,
-                TBText = new Hashtable(),
-                TBAt = new Hashtable()
-            };
-            return data;
+            return new Msg();
         }
 
         /// <summary>
@@ -47,22 +42,44 @@ namespace AIO
             /// 消息类型
             /// </summary>
             [JsonProperty(PropertyName = "msgtype")] [JsonConverter(typeof(StringEnumConverter))]
-            public MsgType Type = MsgType.empty;
+            private MsgType Type = MsgType.empty;
 
             /// <summary>
             /// 文本类型
             /// </summary>
-            [JsonProperty(PropertyName = "text")] public Hashtable TBText;
+            [JsonProperty(PropertyName = "text")] private Hashtable TBText;
 
             /// <summary>
-            /// At类型
+            /// At 类型
             /// </summary>
-            [JsonProperty(PropertyName = "at")] public Hashtable TBAt;
+            [JsonProperty(PropertyName = "at")] private Hashtable TBAt;
 
             /// <summary>
-            /// At类型
+            /// link 类型
             /// </summary>
-            [JsonProperty(PropertyName = "link")] public Hashtable TBLink;
+            [JsonProperty(PropertyName = "link")] private Hashtable TBLink;
+
+            /// <summary>
+            /// actionCard 类型
+            /// </summary>
+            [JsonProperty(PropertyName = "actionCard")]
+            private Hashtable TBActionCard;
+
+            /// <summary>
+            /// feedCard 类型
+            /// </summary>
+            [JsonProperty(PropertyName = "feedCard")]
+            private Hashtable TBFeedCard;
+
+            /// <summary>
+            /// 转换为Json
+            /// </summary>
+            /// <param name="msg">钉钉 msg</param>
+            /// <returns>json string</returns>
+            public static implicit operator string(Msg msg)
+            {
+                return AHelper.Json.Serialize(msg, JsonSetting);
+            }
 
             /// <summary>
             /// 设置 被@人的手机号。
@@ -102,9 +119,11 @@ namespace AIO
             /// <param name="content">内容</param>
             public void ToText(string content)
             {
-                if (TBText is null) TBText = new Hashtable();
                 Type = MsgType.text;
-                TBText["content"] = content;
+                TBText = new Hashtable
+                {
+                    ["content"] = content
+                };
             }
 
             /// <summary>
@@ -116,12 +135,14 @@ namespace AIO
             /// <param name="picUrl">消息链接</param>
             public void ToLink(string title, string content, string messageUrl, string picUrl = null)
             {
-                if (TBLink is null) TBLink = new Hashtable();
                 Type = MsgType.link;
-                TBLink["title"] = title;
-                TBLink["text"] = content;
-                TBLink["messageUrl"] = messageUrl;
-                TBLink["picUrl"] = picUrl;
+                TBLink = new Hashtable
+                {
+                    ["title"] = title,
+                    ["text"] = content,
+                    ["messageUrl"] = messageUrl,
+                    ["picUrl"] = picUrl
+                };
             }
 
             /// <summary>
@@ -131,23 +152,91 @@ namespace AIO
             /// <param name="content">内容</param>
             public void ToMarkdown(string title, string content)
             {
-                if (TBLink is null) TBLink = new Hashtable();
                 Type = MsgType.markdown;
-                TBLink["title"] = title;
-                TBLink["text"] = content;
+                TBLink = new Hashtable
+                {
+                    ["title"] = title,
+                    ["text"] = content
+                };
             }
 
+            /// <summary>
+            /// 消息类型，此时固定为：actionCard
+            /// </summary>
+            /// <param name="title">首屏会话透出的展示内容</param>
+            /// <param name="content">markdown格式的消息</param>
+            /// <param name="singleTitle">单个按钮的标题</param>
+            /// <param name="singleURL">点击消息跳转的URL</param>
             public void ToActionCard(
-                string title, string content, string btnOrientation,
+                string title, string content,
                 string singleTitle, string singleURL)
             {
-                if (TBLink is null) TBLink = new Hashtable();
                 Type = MsgType.actionCard;
-                TBLink["title"] = title;
-                TBLink["text"] = content;
-                TBLink["singleTitle"] = singleTitle;
-                TBLink["singleURL"] = singleURL;
-                TBLink["btnOrientation"] = singleURL;
+                TBActionCard = new Hashtable
+                {
+                    ["title"] = title,
+                    ["text"] = content,
+                    ["singleURL"] = singleURL,
+                    ["singleTitle"] = singleTitle
+                };
+            }
+
+
+            /// <summary>
+            /// 消息类型，此时固定为：actionCard
+            /// </summary>
+            /// <param name="title">首屏会话透出的展示内容</param>
+            /// <param name="content">markdown格式的消息</param>
+            /// <param name="buttons">按钮 ket:按钮标题 value:点击按钮触发的URL</param>
+            /// <param name="btnOrientation">0：按钮竖直排列 other：按钮横向排列</param>
+            public void ToActionCard(
+                string title, string content, IDictionary<string, string> buttons, int btnOrientation = 0)
+            {
+                Type = MsgType.actionCard;
+                TBActionCard = new Hashtable
+                {
+                    ["title"] = title,
+                    ["text"] = content,
+                    ["btns"] = new List<Hashtable>(),
+                    ["btnOrientation"] = (btnOrientation == 0 ? 0 : 1).ToString()
+                };
+
+                foreach (var item in buttons)
+                {
+                    ((List<Hashtable>)TBActionCard["btns"]).Add(new Hashtable
+                    {
+                        ["title"] = item.Key,
+                        ["actionURL"] = item.Value
+                    });
+                }
+            }
+
+
+            /// <summary>
+            /// 消息类型，此时固定为：feedCard
+            /// </summary>
+            /// <param name="links">
+            /// item1[title]:单条信息文本
+            /// item1[messageURL]:点击单条信息到跳转链接
+            /// item1[picURL]:单条信息后面图片的URL
+            /// </param>
+            public void ToFeedCard(IEnumerable<Tuple<string, string, string>> links)
+            {
+                Type = MsgType.feedCard;
+                TBFeedCard = new Hashtable
+                {
+                    ["links"] = new List<Hashtable>(),
+                };
+
+                foreach (var item in links)
+                {
+                    ((List<Hashtable>)TBFeedCard["links"]).Add(new Hashtable
+                    {
+                        ["title"] = item.Item1,
+                        ["messageURL"] = item.Item2,
+                        ["picURL"] = item.Item3
+                    });
+                }
             }
         }
 
@@ -180,6 +269,11 @@ namespace AIO
             /// actionCard
             /// </summary>
             actionCard = 4,
+
+            /// <summary>
+            /// feedCard
+            /// </summary>
+            feedCard = 5,
         }
 
         #region Link
@@ -227,22 +321,5 @@ namespace AIO
         }
 
         #endregion
-
-        /// <summary>
-        /// 发送消息
-        /// </summary>
-        /// <param name="remote">远端地址</param>
-        /// <param name="data">数据</param>
-        /// <param name="secret">签名密钥</param>
-        /// <returns><see cref="IExecutor"/>执行器</returns>
-        public static IExecutor SendData(string remote, Msg data, string secret = null)
-        {
-            var option = new PrCurl.Option();
-            option.include = true;
-            option.AddAuthorization();
-            option.AddContentType();
-            option.AddCharset();
-            return PrCurl.Post(GetSign(remote, secret), AHelper.Json.Serialize(data, JsonSetting), option);
-        }
     }
 }
--- Tools~/Test/Test.Process/Program.cs
+++ Tools~/Test/Test.Process/Program.cs
@@ -1,63 +1,23 @@
-﻿using AIO;
-using System;
-using System.Collections;
-using System.Collections.Concurrent;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace AIO
 {
-    /// <summary>
-    /// 这是一个密封类 GenericClosingException，继承自 Exception
-    /// </summary>
-    public sealed class GenericClosingException : Exception
-    {
-        /// <summary>
-        /// 构造函数
-        /// </summary>
-        public GenericClosingException(string message) : base(message)
-        {
-        }
-
-        /// <summary>
-        /// 构造函数
-        /// </summary>
-        public GenericClosingException(Type open, Type closed) : base(
-            $"Open-constructed type '{open}' is not assignable from closed-constructed type '{closed}'.")
-        {
-        }
-    }
-
     class Program
     {
         private static void Main(string[] args)
         {
-            const string remote =
-                "https://oapi.dingtalk.com/robot/send?access_token=ea1d6a0e15d732e40fac6e2005d19f603ff7e61fe70fa8128c31753c281db8e1";
-            const string secret = "SEC4e0020badd524de7dd33198770cb5e4bb07bda30ee56dd405b763902e682ca5a";
+            const string remote = "https://oapi.dingtalk.com/robot/send?access_token=xxx";
+            const string secret = "xxx";
 
-            var data = PrDingding.CreateMSG();
-
-            // data.ToLink("Unity Common 扩展包", "C# Basic universal library", "https://github.com/AIO-GAME/Common",
-            //     "https://github.com/AIO-GAME/Common/blob/main/Documentation~/Logo.svg");
-            // data.ToMarkdown("天气",
-            //     "#### 天气 @150XXXXXXXX \n > 9度，西北风1级，空气良89，相对温度73%\n > ![screenshot](https://img.alicdn.com/tfs/TB1NwmBEL9TBuNjy1zbXXXpepXa-2400-1218.png)\n > ###### 10点20分发布 [天气](https://www.dingtalk.com) \n");
-
-            data.ToActionCard(
-                "Unity Common 扩展包",
-                "C# Basic universal library",
-                "",
-                "",
-                "");
-
-            data.SetAtAll(false);
-
-            PrDingding.SendData(remote, data, secret).Sync();
+            var data = PrDingding.CreateData();
+            PrDingding.SendMarkdown("title", "content", remote, secret).Sync();
+            PrDingding.SendLink("title", "content", "messageUrl", "picUrl", remote, secret).Sync();
+            PrDingding.SendText("content", remote, secret).Sync();
+            PrDingding.SendActionCard("title", "content", "singleTitle", "singleURL", remote, secret).Sync();
+            PrDingding.SendActionCard("title", "content", new Dictionary<string, string>(), 1, remote, secret).Sync();
+            PrDingding.SendFeedCard(new List<Tuple<string, string, string>>(), remote, secret).Sync();
+            PrDingding.SendData(data, remote, secret).Sync();
             Console.Read();
         }
     }
