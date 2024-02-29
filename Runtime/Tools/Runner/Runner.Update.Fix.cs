140ce7eed5349abebadefec181ce3e6aa28aaa4c
Starfire-Corporation-Win <1398581458@qq.com>
2024-02-29 16:18:38 +0800

    ✨ Runner Support FixedUpdate LateUpdate

--- Runtime/Tools/Runner/Runner.Update.Fix.cs
+++ Runtime/Tools/Runner/Runner.Update.Fix.cs
@@ -6,6 +6,7 @@ using Cysharp.Threading.Tasks;
 
 #if (ENABLE_FIXEDUPDATE_FUNCTION_CALLBACK)
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace AIO
@@ -19,7 +20,7 @@ namespace AIO
         /// <summary>
         /// 在FixedUpdate更新
         /// </summary>
-        public static void ExecuteInFixedUpdate(Action action)
+        public static void UpdateFixed(Action action)
         {
             if (action == null) throw new ArgumentNullException(nameof(action));
 
@@ -38,6 +39,32 @@ namespace AIO
             public void FixedUpdate()
             {
                 if (noActionQueueToExecuteFixedUpdateFunc) return;
+                //清空队列中 残留的操作函数
+                lock (actionQueuesFixedUpdateFunc)
+                {
+                    if (actionQueuesFixedUpdateFunc.Count == 0) return;
+                    noActionQueueToExecuteFixedUpdateFunc = true; //并开启执行状态
+                    lock (mActionCopiedQueueFixedUpdateFunc)
+                    {
+                        mActionCopiedQueueFixedUpdateFunc.AddRange(actionQueuesFixedUpdateFunc); //将等待队列中的操作 复制到执行队列中
+                        actionQueuesFixedUpdateFunc.Clear();
+                        var tempArray = Delegate.Combine(mActionCopiedQueueFixedUpdateFunc.ToArray());
+                        mActionCopiedQueueFixedUpdateFunc.Clear();
+#if SUPPORT_UNITASK
+                        Action().ToUniTask().Preserve().SuppressCancellationThrow();
+#else
+                        StartCoroutine(Action());
+#endif
+                        return;
+
+                        IEnumerator Action()
+                        {
+                            tempArray?.DynamicInvoke();
+                            tempArray = null;
+                            yield break;
+                        }
+                    }
+                }
             }
         }
     }
--- Runtime/Tools/Runner/Runner.Update.Late.cs
+++ Runtime/Tools/Runner/Runner.Update.Late.cs
@@ -5,6 +5,7 @@ using Cysharp.Threading.Tasks;
 
 #if (ENABLE_LATEUPDATE_FUNCTION_CALLBACK)
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace AIO
@@ -17,7 +18,7 @@ namespace AIO
         /// <summary>
         /// 在LateUpdate更新
         /// </summary>
-        public static void ExecuteInLateUpdate(Action action)
+        public static void UpdateLate(Action action)
         {
             if (action == null) throw new ArgumentNullException(nameof(action));
 
@@ -33,6 +34,33 @@ namespace AIO
             public void LateUpdate()
             {
                 if (noActionQueueToExecuteLateUpdateFunc) return;
+                
+                //清空队列中 残留的操作函数
+                lock (actionQueuesLateUpdateFunc)
+                {
+                    if (actionQueuesLateUpdateFunc.Count == 0) return;
+                    noActionQueueToExecuteLateUpdateFunc = true; //并开启执行状态
+                    lock (mActionCopiedQueueLateUpdateFunc)
+                    {
+                        mActionCopiedQueueLateUpdateFunc.AddRange(actionQueuesLateUpdateFunc); //将等待队列中的操作 复制到执行队列中
+                        actionQueuesLateUpdateFunc.Clear();
+                        var tempArray = Delegate.Combine(mActionCopiedQueueLateUpdateFunc.ToArray());
+                        mActionCopiedQueueLateUpdateFunc.Clear();
+#if SUPPORT_UNITASK
+                        Action().ToUniTask().Preserve().SuppressCancellationThrow();
+#else
+                        StartCoroutine(Action());
+#endif
+                        return;
+
+                        IEnumerator Action()
+                        {
+                            tempArray?.DynamicInvoke();
+                            tempArray = null;
+                            yield break;
+                        }
+                    }
+                }
             }
         }
     }
