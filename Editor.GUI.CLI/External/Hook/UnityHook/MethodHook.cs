b7a6a6848ffa9daf4cc7153fc8e445c43d468671
Starfire-Corporation-Win <1398581458@qq.com>
2024-03-21 18:05:00 +0800

    ✨ 新增调试标签功能

--- Editor.GUI.CLI/External/Hook/UnityHook/MethodHook.cs
+++ Editor.GUI.CLI/External/Hook/UnityHook/MethodHook.cs
@@ -10,6 +10,7 @@ using System.Runtime.InteropServices;
 using DotNetDetour;
 using UnityEngine;
 #if UNITY_EDITOR
+using System.Reflection.Emit;
 using UnityEditor;
 #endif
 
@@ -207,15 +208,21 @@ namespace MonoHook
             if (proxyMethod != null)
             {
                 methodName = $"{proxyMethod.DeclaringType?.Name}.{proxyMethod.Name}";
-                var codeSize = proxyMethod.GetMethodBody().GetILAsByteArray().Length;
-                if (codeSize < minMethodBodySize)
-                    Debug.LogWarning(
-                        $"WRANING: size of method body[{methodName}] is too short({codeSize}), will random crash on IL2CPP release mode, please fill some dummy code inside");
+                if (proxyMethod is DynamicMethod)
+                {
+                }
+                else
+                {
+                    var codeSize = proxyMethod.GetMethodBody().GetILAsByteArray().Length;
+                    if (codeSize < minMethodBodySize)
+                        Debug.LogWarning(
+                            $"WRANING: size of method body[{methodName}] is too short({codeSize}), will random crash on IL2CPP release mode, please fill some dummy code inside");
 
-                if ((proxyMethod.MethodImplementationFlags & MethodImplAttributes.NoOptimization) !=
-                    MethodImplAttributes.NoOptimization)
-                    throw new Exception(
-                        $"WRANING: method [{methodName}] must has a Attribute `MethodImpl(MethodImplOptions.NoOptimization)` to prevent code call to this optimized by compiler(pass args by shared stack)");
+                    if ((proxyMethod.MethodImplementationFlags & MethodImplAttributes.NoOptimization) !=
+                        MethodImplAttributes.NoOptimization)
+                        throw new Exception(
+                            $"WRANING: method [{methodName}] must has a Attribute `MethodImpl(MethodImplOptions.NoOptimization)` to prevent code call to this optimized by compiler(pass args by shared stack)");
+                }
             }
 #endif
         }
---
+++ Runtime/Attribute/ProfilerScopeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+/// <summary>
+/// 用于标记方法的性能分析器范围
+/// </summary>
+[AttributeUsage(
+    AttributeTargets.Method |
+    AttributeTargets.Constructor |
+    AttributeTargets.Assembly |
+    AttributeTargets.Property,
+    Inherited = false)]
+[Conditional("UNITY_EDITOR"), DebuggerNonUserCode]
+public class ProfilerScopeAttribute : Attribute
+{
+}
